b3e96468c824841c9904c23ad96d23c5f1441746
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2022-02-03 19:05:13 +0530

    added main menu to the game

--- main.py
+++ main.py
@@ -1,1 +1,48 @@
 # contains main code
+
+from utils import *
+import sys
+import inquirer
+
+def new_game():
+    print("Starting New Game...", end="\r")
+    
+    # chapter_1()
+
+def exit():
+    print("Exiting Game...")
+    sys.exit(0)
+
+def menu():
+    question = [
+        inquirer.List(
+            'menu',
+            message="What do you want to do?",
+            choices = ['New Game', 'Load Game', 'About', 'Exit'],
+        )
+    ]
+    answer = inquirer.prompt(question)
+
+    if answer['menu'] == 'Exit':
+        exit()
+    elif answer['menu'] == 'New Game':
+        new_game()
+
+def main():
+    # printing the game title
+    print(
+"""
+█▀█ █▀█ █▄▀ █▀▀ █▀▄▀█ █▀█ █▄░█
+█▀▀ █▄█ █░█ ██▄ █░▀░█ █▄█ █░▀█
+
+█▀█ █░░ █▄█ █▀▄▀█ █▀█ █ ▄▀█
+█▄█ █▄▄ ░█░ █░▀░█ █▀▀ █ █▀█
+"""
+    )
+
+    # show the game menu
+    menu()
+
+
+if __name__ == '__main__':
+    main()
