cf7808eb4888692c6611ab4af85b0899c12653a4
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2022-02-12 19:56:26 +0530

    test commit

--- chapters/chapter_1.py
+++ chapters/chapter_1.py
@@ -2,10 +2,8 @@ from utils import *
 from rich.console import Console
 import sys, os, time, inquirer
 
+
 def main():
     console = Console()
     console.rule("Chapter 1: New Beginnings")
-    typewrite(
-        ""
-    )
-
+    typewrite("This is a test sentence")
